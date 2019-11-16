99bbe07ee9294e97a5ca84d682223ea8a9471569
alex <alex@alex>
2019-11-16 19:03:30 +0000

    update build-release

--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 d=cadesim-server_"$1"
+if [ "$1" == "" ]; then
+  echo "usage: $0 <version>"
+  exit 1
+fi
 mkdir "$d"
 ./export-to-jar.sh
 cp "cadesim-server.jar" "$d"
@@ -8,3 +12,4 @@ cp -r logs "$d"
 cp LICENSE "$d"
 tar -cjvf "$d".tar.bz2 "$d"
 rm -r "$d"
+exit 0
