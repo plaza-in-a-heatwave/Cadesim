765dc4be7970355a7a2ddd2a85386ebad3694477
alex <alex@alex>
2020-08-06 15:55:12 -0600

    bugfix #48 and #49 cannons and damage/bilge not zeroing when fresh round starts or new game begins

--- src/com/benberi/cadesim/server/model/player/Player.java
+++ src/com/benberi/cadesim/server/model/player/Player.java
@@ -381,6 +381,10 @@ public class Player extends Position {
     	// where to respawn?
     	if (isFirstEntry()) {
     		setFirstEntry(false);
+            // bugfix #48, #49 - zero cannons and damage when just joining or a round restarts
+            tokens.assignCannons(0);
+            vessel.resetDamageAndBilge();
+
     		// start on land side regardless of where
     		// map says we are currently - it's wrong
     		respawnOnLandside(true);
