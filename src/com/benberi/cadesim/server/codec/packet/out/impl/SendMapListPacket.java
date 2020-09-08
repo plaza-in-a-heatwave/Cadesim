a607dfce8d6d61e29091f3c394fa2010056286e2
David <david5292@live.com>
2020-09-08 13:39:57 -0500

    Feature: allow packet to send over screenshots of all maps

--- src/com/benberi/cadesim/server/codec/packet/out/impl/SendMapListPacket.java
+++ src/com/benberi/cadesim/server/codec/packet/out/impl/SendMapListPacket.java
@@ -1,6 +1,15 @@
 package com.benberi.cadesim.server.codec.packet.out.impl;
 
 import com.benberi.cadesim.server.codec.packet.out.OutgoingPacket;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javax.imageio.ImageIO;
+
 import com.benberi.cadesim.server.codec.OutGoingPackets;
 import com.benberi.cadesim.server.codec.util.PacketLength;
 import com.benberi.cadesim.server.config.ServerConfiguration;
@@ -10,14 +19,24 @@ public class SendMapListPacket extends OutgoingPacket{
 		super(OutGoingPackets.SEND_MAPNAMES);
 	}
 
-	@Override
+    @Override
     public void encode() {
-        setPacketLengthType(PacketLength.MEDIUM);
+        setPacketLengthType(PacketLength.MEDIUM); //
         writeByte((byte)ServerConfiguration.getAvailableMaps().size());
         for (int i=0; i<ServerConfiguration.getAvailableMaps().size(); i++)
         {
-        	String map = ServerConfiguration.getAvailableMaps().get(i).replace(".txt", "");
+            String map = ServerConfiguration.getAvailableMaps().get(i).replace(".txt", "");
             writeByteString(map);
+            String mapDir = String.format("maps/%s.png", map);
+            try {
+            	File imageFile = new File(mapDir);
+            	byte[] fileContent = Files.readAllBytes(imageFile.toPath());
+            	writeInt((int)imageFile.length()); //this should be the same as fileContent.length
+            	writeBytes(fileContent); 
+            } catch (IOException e) {
+//              e.printStackTrace();
+                writeInt(0);
+            }
         }
         setLength(getBuffer().readableBytes());
     }
