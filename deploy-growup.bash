3a9baff1b1966768482f944f84abfe795b52bc24
alex <alex@alex>
2020-10-31 00:09:11 +0000

    fix deploy-growup.bash not working on windows

--- deploy-growup.bash
+++ deploy-growup.bash
@@ -3,23 +3,33 @@
 
 # == assumes you have already run the gradle release script. ==
 # == assumes you are running the repo under git.
-ROOT="$(realpath "$(dirname "$0")")"
 
-# 0. save current branch
-CURRENTBRANCH="$(git branch --show-current)"
+# {}:   prevents file disappearing on checkout: https://stackoverflow.com/a/2358491
+# () &: without these windows refuses to checkout original branch (as deploy-growup.bash is locked)
+({
+    ROOT="$(realpath "$(dirname "$0")")"
 
-# 1. change to release branch
-git checkout release --
+    # 0. save current branch
+    CURRENTBRANCH="$(git branch --show-current)"
 
-# 2. add latest to growup dir
-rm -r growup
-mkdir growup
-cp -r release/* growup/
+    # 1. change to release branch
+    if ! git checkout release --; then
+        echo "FATAL ERROR can't checkout release. quitting."
+        exit
+    fi
 
-# 3. apply git
-git add growup/
-git commit -m "release-$(date --iso-8601=s)"
-git push
+    # 2. add latest to growup dir
+    rm -r "$ROOT"/growup
+    mkdir "$ROOT"/growup
+    cp -r release/* "$ROOT"/growup/
 
-# 4. change back
-git checkout "$CURRENTBRANCH"
+    # 3. apply git
+    git add "$ROOT"/growup/
+    git commit -m "release-$(date --iso-8601=s)"
+    git push
+
+    # 4. change back
+    git checkout "$CURRENTBRANCH"
+
+    exit
+}) &
